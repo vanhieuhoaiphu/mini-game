5e86089e05632ff1333d6f020444744169a3a1bd
dark <vanhieuhoaiphu@gmail.com>
2024-08-20 07:44:31 +0700

    initial game project

---
+++ src/game/index.jsx
@@ -0,0 +1,134 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+
+const MiniGame = () => {
+  const status = useMemo(() => ({ start: 0, clear: 1, over: 2 }));
+  const ref = useRef();
+  const inputRef = useRef();
+  const [pointList, setPointList] = useState([]);
+  const [timmer, setTimer] = useState(0);
+  const [start, setStart] = useState(-1);
+  const [startTimer, setStartTimer] = useState(false);
+
+  const handleRenderItem = () => {
+    if (inputRef.current.value >= 1) {
+      setStart(status.start);
+      setTimer(0);
+      setStartTimer(true);
+      const maxY = ref.current.offsetHeight - 50;
+      const maxX = ref.current.offsetWidth - 50;
+      const arr = [];
+      for (let i = 0; i < inputRef.current.value; i++) {
+        arr.push({
+          x: Math.random() * maxX,
+          y: Math.random() * maxY,
+          value: i,
+          checked: false,
+        });
+      }
+      setPointList(arr);
+    } else {
+      alert("Please input a positive integer!");
+    }
+  };
+
+  const handleClick = (item, event) => {
+    if (start != status.over) {
+      event.currentTarget.classList.remove("animation-button");
+      const data = pointList.find((item) => !item?.checked);
+      if (item.value === data.value) {
+        setTimeout(() => {
+          setPointList((pre) => {
+            const updateList = JSON.parse(JSON.stringify(pre));
+            const indexCheck = updateList.findIndex(
+              (item) => item.value === data.value
+            );
+            updateList.splice(indexCheck, 1);
+            if (updateList.length === 0) {
+              setStart(status.clear);
+              setStartTimer(false);
+            }
+            return [...updateList];
+          });
+        }, 1000);
+        const indexCheck = pointList.findIndex(
+          (item) => item.value === data.value
+        );
+        pointList[indexCheck].checked = true;
+        event.currentTarget.classList.add("animation-button");
+      } else {
+        setStart(status.over);
+        setStartTimer(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    let timmer;
+    if (startTimer) {
+      timmer = setInterval(() => {
+        setTimer((prev) => prev + 0.1);
+      }, 100);
+    } else clearInterval(timmer);
+
+    return () => {
+      clearInterval(timmer);
+    };
+  }, [startTimer]);
+
+  return (
+    <div>
+      {start != status.clear && start != status.over ? (
+        <h2>LET'S PLAY</h2>
+      ) : start === status.clear ? (
+        <h2 style={{ color: "green" }}>CLEAR ALL</h2>
+      ) : (
+        <h2 style={{ color: "red" }}>GAME OVER</h2>
+      )}
+
+      <div>
+        <label>Point:</label>
+        <input ref={inputRef} type="number" style={{ marginLeft: "100px" }} />
+      </div>
+      <div>
+        <label>Time:</label>
+        <label style={{ marginLeft: "100px" }}>{timmer.toFixed(1)}s</label>
+      </div>
+      <button onClick={handleRenderItem} style={{ border: "1px solid black",margin:"8px 0" }}>
+        {start === -1 ? "Play" : "Restart"}
+      </button>
+      <div
+        ref={ref}
+        style={{
+          width: "70%",
+          border: "1px solid black",
+          height: "70vh",
+          position: "relative",
+        }}
+      >
+        {pointList.map((item) => (
+          <button
+            key={item.value}
+            style={{
+              position: "absolute",
+              width: "50px",
+              height: "50px",
+              zIndex: inputRef.current.value - item.value,
+              left: item.x,
+              top: item.y,
+              borderRadius: "100%",
+              border: "1px solid black",
+              textAlign: "center",
+              padding: "0px",
+            }}
+            onClick={(event) => {
+              handleClick(item, event);
+            }}
+          >
+            {item.value}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default MiniGame;
